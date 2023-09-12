a57beba05738e479fc2d1070971c01efb93ac390
Bishal Shrestha <71695741+Bi-sh-aL@users.noreply.github.com>
2023-09-12 12:41:29 +0545

    Update TestimonialData.jsx

--- src/components/testimonials/TestimonialData.jsx
+++ src/components/testimonials/TestimonialData.jsx
@@ -28,7 +28,13 @@ const Data=[
         avatar: AVTR2,
         name: "Jessi Pinkman",
         review: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe, nihil accusamus? Cumque repellendus odit provident fugit obcaecati fuga recusandae molestias! Esse eos quis sint commodi facere sunt quod cupiditate quos."
+    },
+    {
+        avatar: AVTR2,
+        name: "Ashol Karki Shrestha",
+        review: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe, nihil accusamus? Cumque repellendus odit provident fugit obcaecati fuga recusandae molestias! Esse eos quis sint commodi facere sunt quod cupiditate quos."
     }
+      
             
 ]
 
